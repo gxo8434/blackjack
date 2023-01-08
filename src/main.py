f20a1f916ade0b972d0d99a0be982bce82a77acb
Sean Reid <seanreid.mail@gmail.com>
2023-01-08 20:23:58 +0000

    abstract player

--- src/main.py
+++ src/main.py
@@ -2,22 +2,24 @@ class Game:
     def __init__(self):
         deck = Deck()
         dealer = Dealer()
-        player = Player()
-
-
+        gambler = Gambler()
 
 class Deck:
     def __init__(self):
         self.init_cards()
         self.shuffle_cards()
 
-class Dealer:
-    def __init__(self):
-        hand = Hand()
-
 class Player:
     def __init__(self):
         hand = Hand()
+
+class Dealer(Player):
+    def __init__(self):
+        super.__init__(self)
+
+class Gambler(Player):
+    def __init__(self):
+        super.__init__(self)
 
 class Hand:
     def __init__(self):
