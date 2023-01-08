5c6d4c6cdaf7cce39605ee22786aef7dea6fbcc5
Sean Reid <seanreid.mail@gmail.com>
2023-01-08 20:17:44 +0000

    skeleton of code

--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
+class Game:
+    def __init__(self):
+        deck = Deck()
+        dealer = Dealer()
+        player = Player()
+
+class Deck:
+    def __init__(self):
+        pass
+
+class Dealer:
+    def __init__(self):
+        pass
+
+class Player:
+    def __init__(self):
+        pass
+
 def main():
     pass
 
